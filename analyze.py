8a079659fbeb350a9c0a18d2abfe118fb6da1575
Junel Solis <junel.solis@abo.fi>
2021-11-05 18:32:42 +0200

    create main script

--- analyze.py
+++ analyze.py
@@ -1,5 +1,32 @@
 import os
-import glob
+import pandas as pd
+from glob import glob
 from skimage import io
 
 import functions
+
+# read images and masks
+first_img_paths = sorted(glob("data/first_images/*"))
+second_img_paths = sorted(glob("data/second_images/*"))
+mask_paths = sorted(glob("data/masks/*"))
+
+results = []
+
+# loop through images
+for idx, p in enumerate(first_img_paths):
+    first_img = io.imread(first_img_paths[idx])
+    second_img = io.imread(second_img_paths[idx])
+    mask = io.imread(mask_paths[idx])
+
+    pearson_r = functions.pearson(first_img, second_img, mask=mask)
+    first_img_fname = os.path.basename(p)
+    second_img_fname = os.path.basename(second_img_paths[idx])
+    result = {
+        "first_image_fname": first_img_fname,
+        "second_image_fname": second_img_fname,
+        "pearson_r": pearson_r,
+    }
+
+    results.append(result)
+
+print(results)
--- functions.py
+++ functions.py
@@ -1,5 +1,36 @@
-def pearson(img1, img2):
-    return
+import numpy as np
+from scipy import stats
+
+
+def pearson(img1, img2, mask=None):
+    """
+    Calculates Pearson's coefficient for two images.
+
+    Parameters
+    ----------
+    img1: n-dimensional ndarray
+        The first image
+    img2: n-dimensional ndarray
+        The second image
+        
+    Returns
+    -------
+    rho
+        The resulting Pearson's coefficient.
+    """
+
+    img1 = img1.reshape(-1).ravel()
+    img2 = img2.reshape(-1).ravel()
+    mask = mask.reshape(-1).ravel()
+
+    img1_mask_indices = np.nonzero(mask)
+    img2_mask_indices = np.nonzero(mask)
+
+    # use only image data within mask
+    img1_m = img1[img1_mask_indices].reshape(-1)
+    img2_m = img2[img2_mask_indices].reshape(-1)
+
+    return np.corrcoef((img1_m, img2_m))[0][1]
 
 
 def manders(img1, img2):
