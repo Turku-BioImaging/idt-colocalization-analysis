246c4819a126165b2ff08f383d0f51f63c2e8759
Junel Solis <junel.solis@gmail.com>
2021-12-28 17:08:29 +0200

    accepts maximum 3d images

--- modules/pearson.py
+++ modules/pearson.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-# from numpy import typing
-
 
 def pearson(img1: np.ndarray, img2: np.ndarray, mask: np.ndarray = None):
-    # print('test')
+
     if img1.ndim != img2.ndim:
         raise BaseException("Images must have the same dimensions.")
 
-    if img1.ndim == 2:
-        img1 = img1.reshape(-1).ravel()
-        img2 = img2.reshape(-1).ravel()
+    if img1.ndim > 3 or img2.ndim > 3:
+        raise BaseException("Images must have at most 3 dimensions.")
 
-    correlation = np.corrcoef(img1, img2)
+    img1_flatten = img1.flatten()
+    img2_flatten = img2.flatten()
+
+    correlation = np.corrcoef(img1_flatten, img2_flatten)
 
     return correlation[0][1]
