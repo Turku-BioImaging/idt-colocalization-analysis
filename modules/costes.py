2200f827374e5c67b26ef479c7041b040b09f761
Junel Solis <junel.solis@gmail.com>
2021-12-31 13:06:20 +0200

    additional costes tests

--- modules/costes.py
+++ modules/costes.py
@@ -1,4 +1,5 @@
 import multiprocessing as mp
+from skimage import img_as_ubyte
 import numpy as np
 
 from pearson import pearson
@@ -11,9 +12,11 @@ def auto_threshold(img1: np.ndarray, img2: np.ndarray, mask: np.ndarray = None):
     if img1.shape != img2.shape:
         raise BaseException("Images must have the same dimensions.")
 
-    threshold, _ = __find_minimum_threshold(img1, img2)
+    threshold = __find_minimum_threshold(img1, img2)
+    img1_thresholded = __threshold(img1, threshold)
+    img2_thresholded = __threshold(img2, threshold)
 
-    return (threshold, 5)
+    return (threshold, img1_thresholded, img2_thresholded)
 
 
 def __find_minimum_threshold(img1: np.ndarray, img2: np.ndarray) -> int:
@@ -22,7 +25,6 @@ def __find_minimum_threshold(img1: np.ndarray, img2: np.ndarray) -> int:
     max_intensity = max([np.max(img1), np.max(img2)])
     threshold_value = max_intensity
 
-    # rho = 1.0
     candidate_thresholds = []
     while threshold_value > 0:
         if threshold_value == 255:
@@ -45,11 +47,12 @@ def __find_minimum_threshold(img1: np.ndarray, img2: np.ndarray) -> int:
 
     minimum_threshold = __get_threshold_from_candidates(candidate_thresholds)
 
-    return (minimum_threshold, 0)
+    return minimum_threshold
 
 
 def __threshold(img: np.ndarray, threshold: int) -> np.ndarray:
-    thresholded = (img > threshold).astype(np.uint8)
+    assert img.dtype == "uint8", "Image must be a numpy array of type uint8."
+    thresholded = img_as_ubyte(img > threshold)
     return thresholded
 
 
--- modules/costes_test.py
+++ modules/costes_test.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from skimage import util
+from skimage import util, io, img_as_ubyte
 
 import costes as costes
 
@@ -26,7 +26,28 @@ def test_auto_threshold_exception_if_image_dimensions_different():
 def test_auto_threshold_returns_threshold_value():
     img1 = np.random.randint(0, 256, size=(200, 200)).astype(np.uint8)
     img2 = (util.random_noise(img1, mode="s&p", amount=0.997) * 255).astype(np.uint8)
-    threshold, _ = costes.auto_threshold(img1, img2)
-    print(threshold)
-    # assert 1 == 0
+    threshold, _, _ = costes.auto_threshold(img1, img2)
     assert threshold >= 0 and threshold < 255
+
+
+def test_auto_threshold_returns_threshold_for_three_dimensional_images():
+    img1 = np.random.randint(0, 256, size=(200, 200, 3)).astype(np.uint8)
+    img2 = (util.random_noise(img1, mode="s&p", amount=0.997) * 255).astype(np.uint8)
+
+    threshold, _, _ = costes.auto_threshold(img1, img2)
+    assert threshold >= 0 and threshold < 255
+
+
+def test_auto_threshold_returns_value_for_test_images():
+    img1 = img_as_ubyte(io.imread("modules/test_data/test1_ch1.png", as_gray=True))
+    img2 = img_as_ubyte(io.imread("modules/test_data/test1_ch2.png", as_gray=True))
+
+    threshold, img1_thresh, img2_thresh = costes.auto_threshold(img1, img2)
+
+    assert threshold == 17
+
+    assert len(np.unique(img1_thresh)) == 2
+    assert len(np.unique(img2_thresh)) == 2
+
+    assert set(np.unique(img1_thresh)) == set([0, 255])
+    assert set(np.unique(img2_thresh)) == set([0, 255])
